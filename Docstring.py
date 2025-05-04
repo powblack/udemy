388849ef6dd6db7831b54d7fb893fe76c2cef43e
powblack <jonathan_c.g1995@hotmail.com>
2025-05-04 10:11:27 -0300

    jhon

---
+++ Docstring.py
@@ -0,0 +1,9 @@
+"""
+Olá mundo, 
+
+Exemplo de Docstring
+
+Pode ser usados como forma de comentário, pois usando o # só é possivel comentar uma linha
+Com a as aspas triplas já conseguimos fazer esse tipo de anotação!!!
+
+"""
