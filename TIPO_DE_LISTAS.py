1e1704f889349a9c5ed31d2427c41409341ee333
powblack <jonathan_c.g1995@hotmail.com>
2025-08-14 19:46:37 -0300

    estudo de strings, continuar

---
+++ TIPO_DE_LISTAS.py
@@ -0,0 +1,14 @@
+"""
+string sao caracteres, ou seja, uma sequencia de caracteres
+com indices, ou seja, cada caracter tem um indice.
+
+tipo de lista  = mutáveis
+
+forma mais comum de criar uma lista é:
+lista = [1, 2, 3, 4, 5] - usando colchetes
+outra forma de criar uma lista é:
+lista = list(range(1, 6)) - usando a função list()
+
+
+
+"""
