5dbbb02c3e47a96a175a828706b536850e4a67c4
powblack <jonathan_c.g1995@hotmail.com>
2025-08-20 08:50:09 -0300

    teste

---
+++ teste_try_except.py
@@ -0,0 +1,12 @@
+
+lista = []
+
+try:
+    numero1 = int(input("Digite um número: "))
+    numero2 = int(input("Digite outro número: "))
+    resultado = numero1 + numero2
+
+    print(f"O resultado da soma é: {resultado}")
+
+except Exception as e:
+    ...
