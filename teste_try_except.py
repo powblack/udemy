1c600303a65340f6a2e767e8101394e5a071553d
powblack <jonathan_c.g1995@hotmail.com>
2025-08-22 07:31:22 -0300

    teste

--- teste_try_except.py
+++ teste_try_except.py
@@ -1,12 +1,20 @@
 
 lista = []
+start = input("Digite 'start' para iniciar o programa: ")
 
-try:
-    numero1 = int(input("Digite um número: "))
-    numero2 = int(input("Digite outro número: "))
-    resultado = numero1 + numero2
+while True:
+    if start.lower() != 'start':
+        print("Comando inválido. Tente novamente.")
+        start = input("Digite 'start' para iniciar o programa: ")
+        continue
+    try:
+        numero1 = int(input("Digite um número: "))
+        lista.append(numero1)
+        print(f"Números digitados: {lista}")
+        continue
 
-    print(f"O resultado da soma é: {resultado}")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
 
-except Exception as e:
-    ...
+
+    
