0aafd63525012f6de0f9c337c31e223a1f9442fb
powblack <jonathan_c.g1995@hotmail.com>
2025-09-02 07:27:54 -0300

    funções

--- inicio_com_def.py
+++ inicio_com_def.py
@@ -5,6 +5,13 @@ replicar determinada ação ao longo do seu codigo
 e retornar um valor específico.
 
 por padrao as funções retornam valor= none
+
+
+creating functions allows you to write code once and reuse it in multiple locations, 
+promoting efficiency and reducing redundancy in your programming. 
+This not only saves time but also makes your code easier to manage and maintain.
+
+
 """
 
 def funcao(): #nome da função, dentro de () é os parametros
@@ -52,3 +59,22 @@ while True:
     saudacao(nome)
     opcao()
     
+"""
+More about DEF
+
+example below:
+
+def multiplo_de(numero, multiplo):
+    resultado = numero % multiplo == 0
+    print(f'{numero} é múltiplo de {multiplo}?', end=' ')
+    print(resultado)
+
+a linha resultado = numero % multiplo == 0, confere se o resto do numero pelo outro for igual a 0, essa linha de 
+codigo retorna um valor boleano TRUE ou FALSE.
+
+a linha de codigo print(f'{numero} é múltiplo de {multiplo}?', end=' '), o end=" ", faz com que o output
+no terminal fique na mesma linha do print, ou seja, ele evita a quebra de linha, pois quando der um input
+o valor iria para linha de baixo, com o end=" " faz com que o valor input fique na mesma linha do output.
+
+
+"""
